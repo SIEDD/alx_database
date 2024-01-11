d811873753244a03f4b5e140704a5fdec7cf129b
siedd <saidmwacharo8@gmail.com>
2024-01-11 23:12:20 +0300

    unique id

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- script that creates table with unique ID
-CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
