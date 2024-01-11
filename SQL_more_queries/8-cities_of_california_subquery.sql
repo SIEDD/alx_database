901da2e16d69f56dd62f6eadeeb954f0be295edc
siedd <saidmwacharo8@gmail.com>
2024-01-11 23:48:01 +0300

    SELECT

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- script that lists all cities of carlifornia
-SELECT cities.id, cities.name, FROM cities, states WHERE cities.states_id = states.id AND states.name = 'Carlifornia';
+SELECT cities.id, cities.name, FROM cities, states WHERE cities.states_id = states.id AND states.name = 'Carlifornia'
 ORDER BY cities.id ASC;
